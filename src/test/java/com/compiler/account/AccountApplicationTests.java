3f8e87067c3065ba327fca4307b1564ca5dc1427
umang <t-umang1.yadav@paytmmoney.com>
2022-12-06 11:49:18 +0530

    Basic Set Up of Application and CI Pipeline

---
+++ src/test/java/com/compiler/account/AccountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.compiler.account;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.testng.annotations.Test;
+
+@SpringBootTest
+class AccountApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
